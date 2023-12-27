41680bf20699011f089c0a49af59b71f690edcd3
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2023-12-27 09:50:53 -0300

    Add initial project files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarters">
+      <Formulario />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import BarraLateral from './components/BarraLateral.vue'
+import Formulario from './components/Formulario.vue';
+
+defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    Formulario
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>
+      <img src="..\assets\images\logo.png" alt="">
+    </h1>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+
+defineComponent({
+  name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+  header {
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+  }
+  @media only screen and (max-width: 768px) {
+    header {
+      padding: 2.5rem;
+      height: auto;
+    }
+  }
+</style>
